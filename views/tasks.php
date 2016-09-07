59193ff9b18f9e07937860afd0d4e592784f4f20
evan stewart <stewarea@gmail.com>
2016-09-07 07:06:12 -0700

    add twig

---
+++ views/tasks.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+    <title>To Do List</title>
+</head>
+<body>
+    <h1>To Do List</h1>
+
+    {% if tasks is not empty %}
+        <p> Here are your tasks </p>
+    {% endif %}
+
+</body>
+</html>
