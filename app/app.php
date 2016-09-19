7809f505c028c59a178960cf906715c9bb1461e2
Evan Stewart <stewarea@gmail.com>
2016-09-19 09:24:52 -0700

    initial commit

--- app/app.php
+++ app/app.php
@@ -1,67 +1,53 @@
 <?php
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
-
-    session_start();
-    if (empty($_SESSION['list_of_tasks'])) {
-    $_SESSION['list_of_tasks'] = array();
-}
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
+    $server = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
-    $output = "";
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
-    $all_tasks = Task::getAll();
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
 
-    if(!empty($all_tasks)) {
-      $output = $output . "
-        <h1>To Do List</h1>
-        <p>Here are all your tasks:</p>
-      "
-    }
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
 
-    foreach ($all_tasks as $task) {
-        $output = $output . "<p>" . $task->getDescription() . "</p>";
-    }
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
 
-    $output = $output . "
-            <form action='/tasks' method='post'>
-                <label for='description'>Task Description</label>
-                <input id='description' name='description' type='text'>
-
-                <button type='submit'>Add task</button>
-            </form>
-        ";
-
-    $output .= "
-           <form action='/delete_tasks' method='post'>
-               <button type='submit'>Clear</button>
-           </form>
-       ";
-
-    return $output;
-});
-    $app->post("/tasks", function() {
+    $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-        ";
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-
-    $app->post("/delete_tasks", function() {
-
+    $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-
-        return "
-            <h1>List Cleared!</h1>
-            <p><a href='/'>Home</a></p>
-        ";
+        return $app['twig']->render('index.html.twig');
     });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
     return $app;
 ?>
